d18fd1c43f3b6afe474ec30f8c7cc68d3caf4ea8
Andrei Sousa <andrei.sousa@einride.tech>
2021-10-21 21:49:41 +0200

    🚧 matchmaking based on users got from slack

--- src/algos.rs
+++ src/algos.rs
@@ -1,10 +1,10 @@
 use mongodb::Client;
 use rand::{prelude::SliceRandom, thread_rng};
+use tracing::log::info;
 
 use crate::{
     db,
-    db::{Channel, User},
-    slack,
+    slack::{self, get_channel_users},
 };
 
 pub async fn matchmake() -> anyhow::Result<()> {
@@ -15,31 +15,35 @@ pub async fn matchmake() -> anyhow::Result<()> {
     let channels = db::list_channels(db).await?;
 
     for channel in channels {
-        let mut users = get_channels_users(channel).await?;
+        let mut users = get_channel_users(&channel.channel_id).await?;
 
         // Shuffle people
         users.shuffle(&mut thread_rng());
 
         // Not enough people to pair
         if users.len() < 2 {
-            return Ok(());
+            info!("not enough ppl");
+            continue;
         }
 
-        let pairs: Vec<&[User]> = users.chunks(2).collect();
+        info!("chunking pairs");
+        let pairs: Vec<&[String]> = users.chunks(2).collect();
 
         if pairs.is_empty() {
-            return Ok(());
+            info!("empty pairs");
+            continue;
         }
 
         // Just one pair, handle naively
         if pairs.len() < 2 {
-            message_pair(pairs[0]).await;
-            return Ok(());
+            info!("one pair");
+            message_pair(pairs[0]).await?;
+            continue;
         }
 
         // Send message to pairs
         for pair in pairs.iter().take(pairs.len() - 2) {
-            message_pair(pair).await;
+            message_pair(pair).await?;
         }
 
         if pairs[pairs.len() - 1].len() < 2 {
@@ -48,71 +52,59 @@ pub async fn matchmake() -> anyhow::Result<()> {
                 &pairs[pairs.len() - 2][0],
                 &pairs[pairs.len() - 2][1],
             )
-            .await;
+            .await?;
         } else {
             // second to last pair
-            message_pair(pairs[pairs.len() - 2]).await;
+            message_pair(pairs[pairs.len() - 2]).await?;
 
             // Last pair
-            message_pair(pairs[pairs.len() - 1]).await;
+            message_pair(pairs[pairs.len() - 1]).await?;
         }
     }
 
     Ok(())
 }
 
-async fn get_channels_users(_channel: Channel) -> anyhow::Result<Vec<User>> {
-    // TODO: get users from slack
-    let users = Vec::new();
-
-    Ok(users)
-}
-
 // TODO: come up with a couple different message
-async fn message_pair(pair: &[User]) {
+async fn message_pair(pair: &[String]) -> anyhow::Result<()> {
     if let [user1, user2] = pair {
-        slack::send_message(
-            user1,
-            format!("This week your fika pair is<@{}>!", user1.user_id),
-        )
-        .await;
-
-        slack::send_message(
-            user2,
-            format!("This week your fika pair is<@{}>!", user2.user_id),
-        )
-        .await;
+        slack::send_message(user1, format!("This week your fika pair is<@{}>!", user1)).await?;
+        slack::send_message(user2, format!("This week your fika pair is<@{}>!", user2)).await?;
     }
+
+    Ok(())
 }
 
-async fn message_trio(user1: &User, user2: &User, user3: &User) {
+async fn message_trio(user1: &String, user2: &String, user3: &String) -> anyhow::Result<()> {
     slack::send_message(
         user1,
         format!(
             "Here is your pair for this week. <@{}> and <@{}>!\nThis time you got an extra buddy! ;)",
-            user2.user_id,
-            user3.user_id
+            user2,
+            user3
         ),
     )
-    .await;
+    .await?;
 
     slack::send_message(
         user2,
         format!(
             "Here is your pair for this week. <@{}> and <@{}>!\nThis time you got an extra buddy! ;)",
-            user1.user_id,
-            user3.user_id
+            user1,
+            user3
         ),
     )
-    .await;
+    .await?;
 
     slack::send_message(
         user3,
         format!(
             "Here is your pair for this week. <@{}> and <@{}>!\nThis time you got an extra buddy! ;)",
-            user1.user_id,
-            user2.user_id
+            user1,
+            user2
         ),
     )
-    .await;
+    .await?;
+
+    Ok(())
 }
--- src/slack.rs
+++ src/slack.rs
@@ -2,23 +2,19 @@ use serde::{Deserialize, Serialize};
 use std::env;
 use tracing::debug;
 
-use crate::db::User;
-
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Eq, PartialOrd, Ord)]
 struct SlackMessage {
     channel: String,
     text: String,
 }
 
-pub async fn send_message(user: &User, text: String) {
+pub async fn send_message(user: &str, text: String) -> anyhow::Result<()> {
     let token = env::var("SLACK_TOKEN").expect("SLACK_TOKEN not set");
 
-    debug!("person {:#?} msg {:#?}", user.user_name, text);
-
-    let User { user_id, .. } = user;
+    debug!("person {:#?} msg {:#?}", user, text);
 
     let msg = SlackMessage {
-        channel: format!("@{}", user_id),
+        channel: format!("@{}", user),
         text,
     };
 
@@ -27,6 +23,51 @@ pub async fn send_message(user: &User, text: String) {
         .bearer_auth(token)
         .json(&msg)
         .send()
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Eq, PartialOrd, Ord)]
+struct ConversationsMembersResponse {
+    members: Vec<String>,
+    next_cursor: Option<String>,
+}
+
+pub async fn get_channel_users(channel_id: &str) -> anyhow::Result<Vec<String>> {
+    let token = env::var("SLACK_TOKEN").expect("SLACK_TOKEN not set");
+
+    let mut users = Vec::new();
+    let mut cursor = None;
+
+    loop {
+        let mut params = vec![("channel", channel_id)];
+
+        if cursor.is_some() {
+            params.push(("cursor", cursor.as_deref().unwrap()))
+        }
+
+        let result = reqwest::Client::new()
+            .post("https://slack.com/api/conversations.members")
+            .bearer_auth(token.clone())
+            .form(&params)
+            .send()
+            .await
+            .unwrap();
+
+        let ConversationsMembersResponse {
+            mut members,
+            next_cursor,
+        }: ConversationsMembersResponse = result.json().await?;
+
+        users.append(&mut members);
+
+        if next_cursor.is_none() {
+            break;
+        }
+
+        cursor = next_cursor;
+    }
+
+    Ok(users)
 }
