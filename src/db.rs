602a494af4e4770755f5c6523b2bb50d1899fd8e
Andrei Sousa <andrei.sousa@einride.tech>
2021-10-27 21:13:29 +0200

    chore: Going sync for all possible cases

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,13 @@
-use mongodb::{Client, Database};
+use mongodb::sync::{Client, Database};
 use std::env;
 
 pub mod channel;
 pub mod user;
 
-async fn get_db() -> anyhow::Result<Database> {
+fn get_db() -> anyhow::Result<Database> {
     let mongo_url = env::var("MONGO_URL").expect("MONGO_URL not present on environment");
 
-    let client = Client::with_uri_str(&mongo_url).await?;
+    let client = Client::with_uri_str(&mongo_url)?;
     let db = client.database("fika");
 
     Ok(db)
--- src/db/channel.rs
+++ src/db/channel.rs
@@ -1,4 +1,3 @@
-use futures_util::stream::StreamExt;
 use mongodb::{
     bson::{self, doc},
     options::UpdateOptions,
@@ -14,54 +13,48 @@ pub struct Channel {
 }
 
 impl Channel {
-    pub async fn save(&self) -> anyhow::Result<()> {
-        let db = get_db().await?;
+    pub fn save(&self) -> anyhow::Result<()> {
+        let db = get_db()?;
 
         let channels = db.collection::<Channel>("channels");
 
         let options = UpdateOptions::builder().upsert(true).build();
 
-        channels
-            .update_one(
-                doc! { "channel_id": self.channel_id.clone() },
-                doc! { "$set": bson::to_document(self)? },
-                options,
-            )
-            .await?;
+        channels.update_one(
+            doc! { "channel_id": self.channel_id.clone() },
+            doc! { "$set": bson::to_document(self)? },
+            options,
+        )?;
 
         Ok(())
     }
 
-    pub async fn delete(channel: &str) -> anyhow::Result<()> {
-        let db = get_db().await?;
+    pub fn delete(channel: &str) -> anyhow::Result<()> {
+        let db = get_db()?;
 
         let channels = db.collection::<Channel>("channels");
 
         // TODO: filter error for channel not there
-        channels
-            .delete_one(doc! { "channel_id": channel }, None)
-            .await?;
+        channels.delete_one(doc! { "channel_id": channel }, None)?;
 
         Ok(())
     }
 
-    pub async fn list() -> anyhow::Result<Vec<Channel>> {
-        let db = get_db().await?;
+    pub fn list() -> anyhow::Result<Vec<Channel>> {
+        let db = get_db()?;
 
         let channels = db.collection::<Channel>("channels");
 
         let channels = channels
-            .find(None, None)
-            .await?
-            .filter_map(|channel| async move {
+            .find(None, None)?
+            .filter_map(|channel| {
                 match channel {
                     Ok(c) => Some(c),
                     // TODO: proper log errors
                     Err(_) => None,
                 }
             })
-            .collect::<Vec<Channel>>()
-            .await;
+            .collect::<Vec<Channel>>();
 
         Ok(channels)
     }
