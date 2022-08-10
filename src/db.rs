516ab1f99dc5dcd8633bddeaf9d7d55c36cf0896
Andrei Sousa <andrei.sousa@einride.tech>
2022-08-10 15:10:53 +0200

    🎨 removing sqlx logging

--- src/db.rs
+++ src/db.rs
@@ -1,19 +1,21 @@
-use sea_orm::DatabaseConnection;
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 use tracing::info;
 
 use crate::Config;
 
 pub async fn get_db(config: &Config) -> anyhow::Result<DatabaseConnection> {
-    info!("db connecting");
+    info!("db connection initializing");
 
     let url = format!(
         "mysql://{}:{}@{}:{}/{}",
         config.db_username, config.db_password, config.db_host, config.db_port, config.db_database,
     );
 
-    let connection = sea_orm::Database::connect(&url).await?;
+    let mut opt = ConnectOptions::new(url);
+    opt.max_connections(3).sqlx_logging(false);
+    let connection = Database::connect(opt).await?;
 
-    info!("Connected to database");
+    info!("connected to database");
 
     Ok(connection)
 }
